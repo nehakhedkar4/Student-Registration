68d2f9f8febb6334e62ebd61c8d14e16da462b86
nehakhedkar4 <nehakhedkar30@gmail.com>
2022-11-15 14:56:23 +0530

    change

--- MyProject/MyApp/admin.py
+++ MyProject/MyApp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import User_Registraion,Student_Application
 
 # Register your models here.
+@admin.register(User_Registraion)
+class UserRegistration(admin.ModelAdmin):
+    list_display = ('username','email','password','password1','profile')
+
+@admin.register(Student_Application)
+class StudentApllication(admin.ModelAdmin):
+    list_display = ('student_username','student_email','Std','Roll_number','School_name','Address','Application_num','status')
